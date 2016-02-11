176320c9c593bbe04708925b58944fdae17f3cb6
Netherwill <yicheng-zhao@uiowa.edu>
2016-02-11 15:10:13 -0800

    first update

---
+++ app/src/main/java/com/example/rln/finalprojectrln/registration.java
@@ -0,0 +1,16 @@
+package com.example.rln.finalprojectrln;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yicheng Zhao on 2/11/2016.
+ */
+public class registration extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+
+    }
+}
